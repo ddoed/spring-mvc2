113753991c7391fce49534fae3f9ebe17dd8b70d
yimnabin <yimnabin20051022@gmail.com>
2025-03-18 14:23:34 +0900

    block

--- thymleaf-basic/src/main/java/hello/thymleaf/basic/BasicController.java
+++ thymleaf-basic/src/main/java/hello/thymleaf/basic/BasicController.java
@@ -140,4 +140,11 @@ public class BasicController {
         return "basic/comments";
     }
 
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
+
+
 }
